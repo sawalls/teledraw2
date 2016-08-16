9bd3247ce02b34509cbc309b5017b36f9faefbaa
Kevin Kozlowski <kevin.kozlowski@gmail.com>
2016-08-16 11:41:12 -0500

    retrieve color from palette

--- client/public/drawCanvas.js
+++ client/public/drawCanvas.js
@@ -1,12 +1,14 @@
 console.log("LOADED drawCanvas.js");
-var context;
+var drawCtx;
+var palette = new Image();
+palette.src = "img_colormap.gif";
 
 app.controller("drawCanvasController", function($scope){
     console.log("Loaded drawCanvasController");
-    context = document.getElementById("drawCanvas").getContext("2d");
+    drawCtx = document.getElementById("drawCanvas").getContext("2d");
 
-    context.fillStyle = "white";
-    context.fillRect(0,0, context.canvas.width, context.canvas.height);
+    drawCtx.fillStyle = "white";
+    drawCtx.fillRect(0,0, drawCtx.canvas.width, drawCtx.canvas.height);
 
     var paint = false;
 
@@ -23,28 +25,28 @@ app.controller("drawCanvasController", function($scope){
     }
 
     function redraw(){
-        context.clearRect(0,0, context.canvas.width, context.canvas.height);
+        drawCtx.clearRect(0,0, drawCtx.canvas.width, drawCtx.canvas.height);
 
-        context.fillStyle = "white";
-        context.fillRect(0,0, context.canvas.width, context.canvas.height);
+        drawCtx.fillStyle = "white";
+        drawCtx.fillRect(0,0, drawCtx.canvas.width, drawCtx.canvas.height);
 
-        context.strokeStyle = "#df4b26";
-        context.lineJoin = "round";
-        context.lineWidth = 5;
+        drawCtx.strokeStyle = "#df4b26";
+        drawCtx.lineJoin = "round";
+        drawCtx.lineWidth = 5;
 
         var segIndex = 0;
         for(var i = 0; i < clickX.length; ++i){
-            context.beginPath();
+            drawCtx.beginPath();
             if(i === clickDrag[segIndex]){//Beginning of a segment
-                context.moveTo(clickX[i]-1,clickY[i]);
+                drawCtx.moveTo(clickX[i]-1,clickY[i]);
                 segIndex++;
             }
             else{
-                context.moveTo(clickX[i-1], clickY[i-1]);
+                drawCtx.moveTo(clickX[i-1], clickY[i-1]);
             }
-            context.lineTo(clickX[i], clickY[i]);
-            context.closePath();
-            context.stroke();
+            drawCtx.lineTo(clickX[i], clickY[i]);
+            drawCtx.closePath();
+            drawCtx.stroke();
         }
     }
 
@@ -74,6 +76,29 @@ app.controller("drawCanvasController", function($scope){
         paint=false;
     });
 
+    var colorCtx = document.getElementById("colorPalette").getContext("2d");
+    colorCtx.drawImage(palette,0,0);
+
+    $scope.paletteClickedHandler = function(event){
+        var clickX = event.offsetX;
+        var clickY = event.offsetY;
+        console.log("Coordinates: (" + clickX + ", " + clickY +")");
+
+        var imgData = colorCtx.getImageData(clickX, clickY, 1, 1).data;
+        var R = imgData[0];
+        var G = imgData[1];
+        var B = imgData[2];
+        console.log(R + " " + G + " " + B);
+
+        var get2digHex = function(num){
+            if(num < 16)
+                return "0" + num.toString(16);
+            else
+                return num.toString(16);
+        }
+        var hexString = "#" + get2digHex(R) + get2digHex(G) + get2digHex(B);
+        console.log(hexString);
+    };
 });
 
 
