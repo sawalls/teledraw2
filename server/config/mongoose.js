0af8edae080b9769c0ab106b15b19a9522674c20
Kevin Kozlowski <Kevin.Kozlowski@gmail.com>
2017-03-19 12:42:21 -0400

    Change Mongo URI to use an environment variable - Fixes #30

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-var uri = "mongodb://pprAdmin:Winchester1427@ds017070.mlab.com:17070/teledraw";
+var uri = process.env.MONGO_URI;
 mongoose.connect(uri);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
